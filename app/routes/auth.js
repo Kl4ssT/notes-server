201e3020263403ac2b40129f32ce3449c0a892f9
Kl4ssT <kl4sst@yandex.ru>
2018-02-02 18:23:25 +0700

    Error messages 3

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -8,14 +8,14 @@ const router = new Router({ prefix: '/auth' });
 router.post('/login', async (ctx) => {
     const { login, password } = ctx.request.body;
 
-    if (!login || login === '') ctx.throw(400, { message: { text: 'Обязательное поле', field: 'login' } });
-    if (!password || password === '') ctx.throw(400, { message: { text: 'Обязательное поле', field: 'password' } });
+    if (!login || login === '') ctx.throw(400, 'Обязательное поле', { field: 'login' });
+    if (!password || password === '') ctx.throw(400, 'Обязательное поле', { field: 'password' });
 
     const user = await models.Users.findOne({ where: { login } });
 
-    if (!user) ctx.throw(404, { message: { text: 'Неверный логин', field: 'login' } });
+    if (!user) ctx.throw(404, 'Неверный логин', { field: 'login' });
 
-    if (!bcrypt.compareSync(password, user.password)) ctx.throw(400, { message: { text: 'Неверный пароль', field: 'password' } });
+    if (!bcrypt.compareSync(password, user.password)) ctx.throw(400, 'Неверный пароль', { field: 'password' });
 
     ctx.body = await getToken(login);
 });
