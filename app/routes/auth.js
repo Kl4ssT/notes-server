061b47e99eb4681bf3b9ceee60f948dabdb17f07
Kl4ssT <kl4sst@yandex.ru>
2018-02-04 11:43:41 +0700

    Authorization 0

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -17,25 +17,22 @@ router.post('/login', async (ctx) => {
 
     if (!bcrypt.compareSync(password, user.password)) ctx.throw(400, JSON.stringify({ message: 'Неверный пароль', field: 'password' }));
 
-    ctx.body = await getToken(login);
+    ctx.body = await getToken(user.login);
 });
 
 router.post('/register', async (ctx) => {
     const { login, password } = ctx.request.body;
 
-    if (!login || login === '') ctx.throw(400, { message: { text: 'Обязательное поле', field: 'login' } });
-    if (!password || password === '') ctx.throw(400, { message: { text: 'Обязательное поле', field: 'password' } });
+    if (!login || login === '') ctx.throw(400, JSON.stringify({ message: 'Обязательное поле', field: 'login' }));
+    if (!password || password === '') ctx.throw(400, JSON.stringify({ message: 'Обязательное поле', field: 'password' }));
 
     const user = await models.Users.findOne({ where: { login } });
 
-    if (user) ctx.throw(400, { message: { text: 'Логин существует', field: 'login' } });
+    if (user) ctx.throw(400, JSON.stringify({ message: 'Пользователь существует', field: 'login' }));
 
-    await models.Users.create({ login, password });
+    const newUser = await models.Users.create({ login, password });
 
-    ctx.body = {
-        login,
-        password
-    };
+    ctx.body = await getToken(newUser.login);
 });
 
 export default router;
